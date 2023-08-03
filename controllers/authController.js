61120ee3bac1cca509eeb9aae5c21121e7b19206
ianSu <iamsu@mirle.com.tw>
2023-08-03 15:05:41 +0800

    update authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -10,7 +10,7 @@ const createUserInfo = (user) => ({
 
 const getAccessToken = (userInfo) => {
   return jwt.sign({ UserInfo: userInfo }, process.env.JWT_SECRET, {
-    expiresIn: '15m'
+    expiresIn: '10m'
   })
 }
 
@@ -23,22 +23,22 @@ const login = async (req, res) => {
 
   const foundUser = await User.findOne({ email }).exec()
 
-  const match = await bcrypt.compare(password, foundUser.password)
-  if (!match) return res.status(401).json({ message: 'Wrong email or password' })
+  if (foundUser) {
+    const match = await bcrypt.compare(password, foundUser.password)
+    if (!match) return res.status(401).json({ message: 'Wrong email or password' })
+    if (!foundUser.active) return res.status(401).json({ message: 'Inactive' })
 
-  if (!foundUser || !foundUser.active) {
-    return res.status(401).json({ message: 'Inactive' })
+    const userInfo = createUserInfo(foundUser)
+    const accessToken = getAccessToken(userInfo)
+    res.json({ accessToken, userInfo })
+  } else {
+    res.status(401).json({ message: 'Wrong email or password' })
   }
-
-  const userInfo = createUserInfo(foundUser)
-  const accessToken = getAccessToken(userInfo)
-
-  res.json({ accessToken, userInfo })
 }
 
 const getUserInfo = (req, res) => {
   jwt.verify(req.params.jwt, process.env.JWT_SECRET, async (err, decoded) => {
-    if (err) return res.status(403).json({ message: 'Forbidden' })
+    if (err) return res.status(401).json({ message: err.message })
     const { id, email } = decoded.UserInfo
     const foundUser = await User.findOne({ _id: id, email }).exec()
 
